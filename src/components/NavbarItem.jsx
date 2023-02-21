f18702d9631ef4625578388460d08fdfee883feb
kopre2012 <kopre2012@gmail.com>
2023-02-21 19:09:04 +0100

    fetch data from tmdb api in the home page

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -13,8 +13,12 @@ export default function NavbarItem({title, param}) {
     <div>
       <Link 
         className={
-          `m-4 hover:text-amber-600 font-semibold p-2 ${genre && genre === param && 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'}`} 
-       href={`/?genre=${param}`}>
+          `m-4 hover:text-amber-600 font-semibold p-2 ${
+            genre && 
+            genre === param && 
+            'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+          }`} 
+          href={`/?genre=${param}`}>
         {title}
       </Link>
     </div>
