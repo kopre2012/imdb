0e9b059480aae8e4917bfa892cf3449bbc33e7dc
kopre2012 <kopre2012@gmail.com>
2023-02-21 15:44:16 +0100

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie enthusiasists who have come together a one-stop destination for all your movie-related needs. 
+      </p>
+
+      <p>
+        Our website is designed to provide you with a comprehensive database of movies from all around the world, along with the latests news, rewiews, and trailers. Our movie database is contsantly updated with new releases, ensuring that you have access to the latest and greatest in the world cinema. You can search for movies by title, director, actor, genre, or release date, making it easy to find the perfect movie for any occasion.
+      </p>
+
+      <p>
+        In addition to our extensive movie, we also offer a platform for movie lovers to connect and share their thoughts on the latest release. Our comunity section includes a forum where you can discuss your favorite films with like-minded individuals and read reviews and ratings from other users. We also have a section dedicated to movie news and trailers, keeping you updated with the latest happenings in the world of cinema. Thank you for visiting our website and we hope you enjoy your time browsing through our movie database. If you have any feedback or suggestion, please feel free to contact us. We are always looking for ways to improve and enhance the user experience on our website. Happy browsing!
+      </p>
+    </div>
+  )
+}
