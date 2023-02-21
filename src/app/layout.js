84c52731bdbffb119fbbf5fad1ac8087d9488773
kopre2012 <kopre2012@gmail.com>
2023-02-21 14:36:13 +0100

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
 import Header from '@/components/Header';
 import './globals.css';
+import Providers from './Providers';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
 
-        {/* SearchBox */}
+          {/* SearchBox */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
